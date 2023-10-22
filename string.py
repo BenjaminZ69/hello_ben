625817b4fe948655ee9385f345ddd75211f64879
BenjaminZ69 <148010760+BenjaminZ69@users.noreply.github.com>
2023-10-22 17:01:25 -0400

    Add files via upload

---
+++ string.py
@@ -0,0 +1,5 @@
+character_name = "Karen"
+character_age = "96"
+
+print("My name is " + character_name)
+print("My age is: " + character_age)
---
+++ string1.py
@@ -0,0 +1,5 @@
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+print(phrase[0])
+print(len(phrase))
+print(phrase.index("A"))
