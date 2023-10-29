dd726d60da02bf4c8599d632b9ebc14811a795cd
BenjaminZ69 <148010760+BenjaminZ69@users.noreply.github.com>
2023-10-29 16:40:06 -0400

    Add files via upload

---
+++ Stanly2.py
@@ -0,0 +1,3 @@
+name = input("Enter youe name?")
+age = input("how odl ar you?")
+print("Hi: " + name +", You are " + age)
---
+++ stanley1.py
@@ -0,0 +1,5 @@
+from math import *
+
+My_Balls = 69
+print(sqrt(144))
+print(9 % 2)
